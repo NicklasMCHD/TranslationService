a1e14bcad3ba71f8af3ca628bd3c65d6b9c8b2e7
NicklasMCHD <NicklasMCHD@live.dk>
2018-06-24 00:55:41 +0000

    Fixed WebService to listen on any local ip

--- backend/main.py
+++ backend/main.py
@@ -9,5 +9,5 @@ import requests_cache
 
 print("Setting up cache")
 requests_cache.install_cache('translation.cache', backend='sqlite')
-web = WebService("localhost", 11000)
+web = WebService("0.0.0.0", 11000)
 web.start()
